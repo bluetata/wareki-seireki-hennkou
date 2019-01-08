fce176431597934402154e0795a50688bd4572cd
bluetata <Sekito.Lv@gmail.com>
2019-01-08 18:10:55 +0800

    Add C sharp version code

---
+++ csharp-version/csharp-version/Seireki2WarekiConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_version
+{
+    class Seireki2WarekiConversion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");　// in processing
+        }
+    }
+}
